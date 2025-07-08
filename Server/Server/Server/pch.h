4d4e428c48cc1d5aaba9d6053b8d95b375d2fed9
jeonghoons <116326836+jeonghoons@users.noreply.github.com>
2025-07-08 17:46:42 +0900

    Init

---
+++ Server/Server/Server/pch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#include <iostream> 
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#include <Windows.h>
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+
+using namespace std;
+
+#include <memory>
+#include <vector>
+#include <set>
+#include <queue>
+#include <unordered_map>
+
+extern thread_local unsigned int Lthreadid;
+
+
+
+#define PORT_NUM 8888
+
